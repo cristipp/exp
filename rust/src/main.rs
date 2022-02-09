e08bc99299e7c1651ab50bf4391838bc45ac4a4a
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-02-09 12:16:56 -0800

    ok

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,30 +1,38 @@
-use std::any::{Any, TypeId};
+use std::any::{Any};
 use std::panic;
 // unstable
 // use std::backtrace::Backtrace;
+
+// https://stackoverflow.com/a/68558313
+
+fn convert_unwind_error<R>(r: Result<R, Box<dyn Any + Send>>) -> Result<R, String> {
+    return match r {
+        Ok(x) => Ok(x),
+        Err(e) => Err(
+            match e.downcast::<String>() {
+                Ok(s) => *s,
+                Err(e) => match e.downcast::<&str>() {
+                    Ok(m1) => m1.to_string(),
+                    Err(_) => "unknown panic cause".to_string(),
+                }
+            }
+        )
+    }
+}
 
 fn unwind() {
     let r0 = panic::catch_unwind(|| {
         return "nothing new";
     });
-    println!("ok {:?}", r0);
+    println!("ok {:?}", convert_unwind_error(r0));
     let r1 = panic::catch_unwind(|| {
-        // panic!("not good")
-        panic!("not good {}", 2)
+        panic!("not good");
     });
-    let r2 = match r1 {
-        Ok(r) => Ok(r),
-        Err(e) => Err(
-            match e.downcast::<String>() {
-                Ok(s) => *s,
-                Err(_) => match e.downcast::<&str>() {
-                    Ok(m1) => m1.to_string(),
-                    Err(_) => "unknown cause".to_string(),
-                }
-            }
-        ),
-    };
-    println!("err {:?}", r2);
+    println!("err {:?}", convert_unwind_error(r1));
+    let r2 = panic::catch_unwind(|| {
+        panic!("not good {}", "apple");
+    });
+    println!("err {:?}", convert_unwind_error(r2));
 }
 
 fn main() {
